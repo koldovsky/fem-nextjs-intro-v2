8fd1945ef2a35d17485d5f9f8d0b208b93385ec9
koldovsky <koldovsky@koldovsky.com>
2023-02-09 20:01:24 +0200

    Part 2

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,14 @@
+'use client'
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 export default function ContactPage() {
-  return (<div>Contact</div>);
+  const router = useRouter();
+  return (
+    <div>
+      <h1>Contact</h1>
+      <Link href="/">Home</Link>
+      <button onClick={() => router.push("/")}>Home</button>
+    </div>
+  );
 }
